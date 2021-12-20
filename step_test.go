5767ea64fd0258c1ee501540d7ad6d6c73644b7e
Chris <github.account@chrigel.net>
2021-12-20 19:08:34 +0100

    Add error handler

---
+++ step_test.go
@@ -0,0 +1,38 @@
+package pipeline
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStep_WithErrorHandler(t *testing.T) {
+	tests := map[string]struct {
+		givenError        error
+		expectedExecution bool
+	}{
+		"GivenHandler_WhenErrorIsNil_ThenDoNotRunHandler": {
+			givenError:        nil,
+			expectedExecution: false,
+		},
+		"GivenHandler_WhenErrorGiven_ThenExecuteHandler": {
+			givenError:        errors.New("error"),
+			expectedExecution: true,
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			executed := false
+			s := NewStepFromFunc("test", func(_ Context) error {
+				return nil
+			}).WithErrorHandler(func(_ Context, err error) error {
+				executed = true
+				return err
+			})
+			err := s.H(nil, Result{Err: tt.givenError})
+			assert.Equal(t, tt.givenError, err)
+			assert.Equal(t, tt.expectedExecution, executed)
+		})
+	}
+}
