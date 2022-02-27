b8b68a37241558b5b5e5969902cad751bbf06873
Chris <github.account@chrigel.net>
2022-02-27 13:00:05 +0100

    Add 'SupplierFromSlice' utility function

--- parallel/supplier.go
+++ parallel/supplier.go
@@ -18,3 +18,21 @@ import (
 //  select { case <-ctx.Done(): return, default: pipelinesChan <- ... }
 // to cancel the supply, otherwise you may leak an orphaned goroutine.
 type PipelineSupplier func(ctx context.Context, pipelinesChan chan *pipeline.Pipeline)
+
+// SupplierFromSlice returns a PipelineSupplier that accepts the given slice of pipeline.Pipeline and iterates over it to feed the channel.
+//
+// Context cancellation is only effective if the channel is limited in size.
+// All pipelines may get executed even if the parent pipeline has been canceled, unless each child pipeline listens for context.Done() in their steps.
+func SupplierFromSlice(pipelines []*pipeline.Pipeline) PipelineSupplier {
+	return func(ctx context.Context, pipelinesChan chan *pipeline.Pipeline) {
+		defer close(pipelinesChan)
+		for _, pipe := range pipelines {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				pipelinesChan <- pipe
+			}
+		}
+	}
+}
